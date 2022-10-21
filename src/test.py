a304858a1da98e5e159d46fc3bb99c77fb9fe2d4
JoshJohnson2001 <91692785+JoshJohnson2001@users.noreply.github.com>
2022-10-20 21:02:19 -0600

    refactor: Change array variable to array string

--- src/test.py
+++ src/test.py
@@ -4,21 +4,9 @@ prolog = Prolog()
 prolog.consult('game_rules.pl')
 
 def LoadMaze():        
-    assertz = Functor("assertz", 1)
+    assertz = Functor('assertz', 1)
     maze = Functor('maze',1)
-    Matriz=[["x","x","x","x","x","x","x","x","x","x","x"],
-            ["x","ar","x","x","ad","ad","ad","inter","ad","inter","x"],
-            ["i","inter","ad","ad","inter","x","x","ab","x","ab","x"],
-            ["x","ab","x","x","x","inter","at","inter","x","ab","x"],
-            ["x","ab","x","x","x","ab","x","ab","x","x","x"],
-            ["x","ab","x","x","inter","inter","x","ab","x","inter","f"],
-            ["x","ab","x","x","ab","x","x","inter","inter","inter","x"],
-            ["x","ab","x","x","ab","x","x","x","ar","x","x"],
-            ["x","ab","x","at","inter","inter","ad","ad","inter","at","x"],
-            ["x","ab","x","ar","x","ab","x","x","ab","ar","x"],
-            ["x","inter","ad","inter","x","inter","ad","x","ab","inter","x"],
-            ["x","x","x","x","x","x","x","x","x","x","x"]]
-    call(assertz(maze(Matriz)))
+    call(assertz(maze("[[x,x,x,x,x,x,x,x,x,x,x],[x,ar,x,x,ad,ad,ad,inter,ad,inter,x],[i,inter,ad,ad,inter,x,x,ab,x,ab,x],[x,ab,x,x,x,inter,at,inter,x,ab,x],[x,ab,x,x,x,ab,x,ab,x,x,x],[x,ab,x,x,inter,inter,x,ab,x,inter,f],[x,ab,x,x,ab,x,x,inter,inter,inter,x],[x,ab,x,x,ab,x,x,x,ar,x,x],[x,ab,x,at,inter,inter,ad,ad,inter,at,x],[x,ab,x,ar,x,ab,x,x,ab,ar,x],[x,inter,ad,inter,x,inter,ad,x,ab,inter,x],[x,x,x,x,x,x,x,x,x,x,x]]")))
 
 def getMaze():        
     maze = Functor('maze',1)
